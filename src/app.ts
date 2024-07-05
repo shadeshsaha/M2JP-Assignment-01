066d61d48cbee16fb952e3521107c65a11a6c4f2
shadeshsaha <shadesh15-8599@diu.edu.bd>
2024-07-06 01:50:30 +0600

    Changes in App, Server files and Added Not Found Feature

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,55 @@
-import cors from "cors";
-import express, { Request, Response } from "express";
-import { orderRoutes } from "./app/modules/Order/order.route";
-import { ProductRoutes } from "./app/modules/product/product.routes";
-
-const app = express();
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { z } from 'zod';
+import notFound from './app/middlewares/notFound';
+import { orderRoutes } from './app/modules/Order/order.route';
+import { ProductRoutes } from './app/modules/product/product.routes';
+const app: Application = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/products", ProductRoutes);
-app.use("/api/orders", orderRoutes);
+// Application Routes
+app.use('/api/products', ProductRoutes);
+app.use('/api/orders', orderRoutes);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Mission To JP - Assignment-01");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Mission To JP - Assignment-01');
 });
+
+// Global Error Handling Middleware
+app.use((err: any, req: Request, res: Response) => {
+  // Handling the Zod error with a professional error message
+  if (err instanceof z.ZodError) {
+    res.status(400).json({
+      success: false,
+      message: `${
+        err?.issues[0]?.message !== 'Required'
+          ? `${err?.issues[0]?.code} : ${err?.issues[0]?.message}`
+          : 'Field is required! Please provide required data.'
+      }`,
+      error: err?.issues,
+    });
+  } else if (err?.code === 11000 && err.keyPattern.name) {
+    res.status(400).json({
+      success: false,
+      message: 'Product name must be unique. The name already exists.',
+    });
+  } else {
+    // Sending JSON response with error details
+    res.status(400).json({
+      success: false,
+      message: err.message || 'Something went wrong!',
+      error: err,
+    });
+  }
+});
+
+// Not Found Route - 404
+app.use(notFound);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./app/config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
 
 async function main() {
   try {
+    // Database Connection - Mongoose
     await mongoose.connect(config.DATABASE_URL as string);
 
     app.listen(config.PORT, () => {
       console.log(
-        `Mission To JP - Assignment-01. App listening on port ${config.PORT}`
+        `Mission To JP - Assignment-01. App listening on port ${config.PORT}`,
       );
     });
   } catch (err) {
-    console.log(err);
+    console.log('Database Connection Error', err);
   }
 }
 
